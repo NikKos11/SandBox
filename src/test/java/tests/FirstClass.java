6be66c0e81c3ced4bea96f7b612ad1d70331461a
NikKos11 <nikkos9009@gmail.com>
2025-12-07 13:44:03 +0300

    HW retest 2

--- src/test/java/tests/FirstClass.java
+++ src/test/java/tests/FirstClass.java
@@ -2,16 +2,20 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import org.w3c.dom.Text;
+
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class FirstClass extends BaseTest {
+    private static final By TEXTCHECK = By.xpath("//*[span='Products']");
+    private static final String TEXTMSG = "Epic sadface: Username and password do not match any user in this service";
 
     @Test
     public void logPass() {
         loginPage.open();
         loginPage.login();
-        boolean productsBtnPresent = browser.findElement(By.xpath("//*[span='Products']")).isDisplayed();
+        boolean productsBtnPresent = browser.findElement(TEXTCHECK).isDisplayed();
         assertTrue(productsBtnPresent);
     }
 
@@ -19,7 +23,7 @@ public class FirstClass extends BaseTest {
     public void logFailed() {
         loginPage.open();
         loginPage.checkErrorMsg();
-        assertEquals(loginPage.checkErrorMsg(), "Epic sadface: Username and password do not match any user in this service");
+        assertEquals(loginPage.checkErrorMsg(), TEXTMSG);
     }
 
 }
